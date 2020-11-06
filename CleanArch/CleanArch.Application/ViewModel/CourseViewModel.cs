62862611734f407c5b9633ee706cf454becd3ce9
Mark Irey <Mark.Irey@hyland.com>
2020-11-06 11:53:52 -0800

    Course service create funcionality, updated CourseViewModel.

--- CleanArch/CleanArch.Application/ViewModel/CourseViewModel.cs
+++ CleanArch/CleanArch.Application/ViewModel/CourseViewModel.cs
@@ -7,6 +7,9 @@ namespace CleanArch.Application.ViewModel
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public IEnumerable<Course> Courses { get; set; }
     }
 }
