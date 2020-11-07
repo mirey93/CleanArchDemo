fb8c2c1818108b2830070db069455927ab8c3973
Mark Irey <Mark.Irey@hyland.com>
2020-11-07 10:11:10 -0800

    Final AutoMapper configuration changes

---
+++ CleanArch/CleanArch.Application/AutoMapper/ViewModelToDomanProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CleanArch.Application.ViewModel;
+using CleanArch.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.AutoMapper
+{
+    public class ViewModelToDomanProfile : Profile
+    {
+        public ViewModelToDomanProfile()
+        {
+            CreateMap<CourseViewModel, CreateCourseCommand>()
+                .ConvertUsing(c => new CreateCourseCommand(c.Name, c.Description, c.ImageUrl));
+        }
+    }
+}
--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -8,6 +8,6 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         void Create(CourseViewModel courseViewModel); 
-        CourseViewModel GetCourses();
+        IEnumerable<CourseViewModel> GetCourses();
     }
 }
